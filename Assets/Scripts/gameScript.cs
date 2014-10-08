707ccb000eb13c81ad85f59cf6cdbb2f6557b37a
RocioB <rociobarlo@gmail.com>
2014-10-08 18:47:15 +0200

    MenuIniciar

---
+++ Assets/Scripts/gameScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Escape)) {
+			reiniciar ();
+		}
+	
+	}
+	public void salir () {
+		Application.Quit ();
+		}
+	public void reiniciar () {
+		Application.LoadLevel ("Menu");
+		}
+	public void Iniciar () {
+		Application.LoadLevel ("nivel_1");
+	}
+}
